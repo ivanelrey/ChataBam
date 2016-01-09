f76eda151dbb6fa68b10d5bc6915c16daff120a4
lefter01 <necrothaft1s@hotmail.com>
2016-01-09 06:29:27 -0800

    logout.php
    delete user on logout

--- logout.php
+++ logout.php
@@ -1,10 +1,12 @@
 <?php
+require './DBconnection.php';
 require 'sessionUnset.php';
 session_start();
 if(isset($_SESSION['username']))
 {
+    $databaseConnection->query("DELETE FROM user WHERE username = '{$_SESSION['username']}'");
     unsetSession();
-    header("Location: index.php");
+    header("Location: index.php");    
 }
 else
 {
